ffa69ba79317784fe5e3fa333cef20bd29cfced9
Daniel Railean <daniel.railean1@gmail.com>
2021-11-26 13:21:12 +0100

    Moved folders

--- Assignment2/TEST/QueenProblemTest.java
+++ Assignment2/TEST/QueenProblemTest.java
@@ -1,10 +1,5 @@
-import com.ddlele.QueenProblem;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
--- Assignment2/src/QueenProblem.java
+++ Assignment2/src/QueenProblem.java
@@ -1,5 +1,3 @@
-package com.ddlele;
-
 import java.util.ArrayList;
 import java.util.List;
 
